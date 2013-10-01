b9c05f240699f25572a6cc6f356534761f55d982
Travis Vachon <travis@copious.com>
2013-10-01 18:41:21 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject parseapp-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1896"]
+                 [ring "1.1.8"]]
+  :plugins [[lein-cljsbuild "0.3.2"]
+            [lein-ring "0.8.3"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
+  :main parseapp-cljs.server
+  :ring {:handler parseapp-cljs.server/app})
+
