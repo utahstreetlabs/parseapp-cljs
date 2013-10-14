157cdac352ba068be91a3911322db477173188a5
Travis Vachon <travis@copious.com>
2013-10-14 13:50:46 -0400

    Lock down core.async dep

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
                  [ring "1.1.8"]]
   :plugins [[lein-cljsbuild "0.3.2"]
             [lein-ring "0.8.3"]]
