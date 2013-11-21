6cbc21c99f89126c25b40c6a9e6df7203a8d9a44
Robert Zuber <rob@zuber.net>
2013-11-21 15:48:49 -0800

    Pull a few utilities up from individual apps.

--- src/cljs/parseapp_cljs/express.cljs
+++ src/cljs/parseapp_cljs/express.cljs
@@ -1,4 +1,5 @@
-(ns parseapp-cljs.express)
+(ns parseapp-cljs.express
+  (:require [parseapp-cljs.parse :refer [User]]))
 
 (def express (js/require "express"))
 (def parse-express-raw-body (js/require "parse-express-raw-body"))
@@ -21,3 +22,32 @@
     (.use app (.-router app))
     (doseq [tag (:static config)] (static app tag))
     app))
+
+(defn render
+  ([response template] (render response template {}))
+  ([response template data] (.render response template (clj->js data))))
+
+(defn render-json
+  ([response] (render-json response 200 {}))
+  ([response data] (render-json response data {}))
+  ([response status data] (.json response status (clj->js data))))
+
+(defn redirect-after-post [response url]
+  (.redirect response 303 url))
+
+(defn current-user []
+  (.current User))
+
+(defn logged-in? []
+  (boolean (current-user)))
+
+(defn force-login
+  ([url-getter]
+    (fn [handler]
+      (fn [request response]
+        (if (logged-in?)
+          (handler request response)
+          (if (.-xhr request)
+            (render-json response 400 {:error "You must be logged in to perform this action."})
+            (redirect-after-post response (url-getter request)))))))
+  ([] (force-login #("/"))))
