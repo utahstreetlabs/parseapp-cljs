634f8fced780de3b494fbb1659cb786dbab63cd2
Travis Vachon <travis@copious.com>
2013-11-24 15:18:19 -0500

    no optimizations when compiling cljs

--- project.clj
+++ project.clj
@@ -14,6 +14,6 @@
     :builds {
       :main {
         :source-paths ["src/cljs"]
-        :compiler {:optimizations :simple
+        :compiler {:optimizations :whitespace
                    :pretty-print true}
         :jar true}}})
