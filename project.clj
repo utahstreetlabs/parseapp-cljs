fc8ef06e5558a1b726f0c184b2abbd0432e5ff2e
Travis Vachon <travis@copious.com>
2013-12-12 12:49:10 -0500

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject parseapp-cljs "0.6.0"
+(defproject parseapp-cljs "0.7.0-SNAPSHOT"
   :description "clojurescript to parse cloud code"
   :url "https://github.com/utahstreetlabs/parseapp-cljs"
   :repositories {
