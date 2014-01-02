37c6c2552b92de0796f76dc6128a3c4536c541ed
Robert Zuber <rob@zuber.net>
2014-01-01 22:47:31 -0800

    Add macro to simplify endpoint def

--- src/clj/parseapp_cljs/express_macros.clj
+++ src/clj/parseapp_cljs/express_macros.clj
@@ -4,3 +4,12 @@
   `(-> (parseapp-cljs.express/new-express-app ~config)
        ~@body
        .listen))
+
+(defmacro go-endpoint [[request response] & body]
+  `(fn [~request ~response]
+     (cljs.core.async.macros/go
+       (try
+         ~@body
+         (catch :default e#
+           (.error js/console (.-stack e#))
+           (.send ~response 500 (.-message e#)))))))
