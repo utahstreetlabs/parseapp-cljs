8dfa894702ce5c28adc0d77b292ea3162cbe669f
Travis Vachon <travis@copious.com>
2014-03-06 15:49:04 -0500

    Add config options for adding error handlers

--- src/cljs/parseapp_cljs/express.cljs
+++ src/cljs/parseapp_cljs/express.cljs
@@ -21,15 +21,18 @@
       (.use app (.cookieParser express cookie-secret))
       (.use app (.cookieSession express {:cookie {:maxAge cookie-max-age}}))
       (.use app (parse-express-cookie-session (clj->js {:cookie {:maxAge cookie-max-age}}))))
+    (when (:locals config) (.locals app (clj->js (:locals config))))
 
     (when (:uses config)
       (doseq [use (:uses config)] (.use app use)))
     (when (:initializer config) ((:initializer config) app))
 
     (.use app (.-router app))
-    (doseq [tag (:static config)] (static app tag))
-    (when (:locals config) (.locals app (clj->js (:locals config))))
 
+    (doseq [tag (:static config)] (static app tag))
+
+    (when (:error-handlers config)
+      (doseq [handler (:error-handlers config)] (.use app handler)))
     app))
 
 (defn render
