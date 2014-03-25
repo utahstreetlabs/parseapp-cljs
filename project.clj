940cc18d0fc83876cbeb952704b2b46475fe1f93
Travis Vachon <travis@copious.com>
2014-03-25 10:27:32 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject parseapp-cljs "0.22.0"
+(defproject parseapp-cljs "0.23.0"
   :description "clojurescript to parse cloud code"
   :url "https://github.com/utahstreetlabs/parseapp-cljs"
   :repositories {
