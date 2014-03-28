486f87c08d5d8f6afb1877cd6ffa696e33cf5ee3
Travis Vachon <travis@copious.com>
2014-03-28 16:53:33 -0700

    Bump to 0.26.0 and forget that awful hack
    that hack was making dates translate to unreadable forms it should die
    and never come back

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject parseapp-cljs "0.25.0"
+(defproject parseapp-cljs "0.26.0"
   :description "clojurescript to parse cloud code"
   :url "https://github.com/utahstreetlabs/parseapp-cljs"
   :repositories {
